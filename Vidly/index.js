02ee9fbcb5dca7b7817d59222e6b7f07ef64d49e
Paulo Matos <paulo.matos@itsector.pt>
2020-05-08 17:50:38 +0100

    vidly api

--- Vidly/index.js
+++ Vidly/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
+
 app.use(express.json());
 
 const genres = [
@@ -7,5 +9,11 @@ const genres = [
     {id: 2, name: 'romance'},
     {id: 3, name: 'action'}
 ]
+
+app.get('api/genres', (req, res) => {
+    app.send(genres);
+});
+
+
 
 
